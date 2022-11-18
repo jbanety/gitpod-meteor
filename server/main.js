8c68e002c5ae9c5763366053e83ca59bcf19b55f
Jean-Baptiste Alleaume <jbanety@gmail.com>
2022-11-18 23:44:09 +0100

    Update main.js

--- server/main.js
+++ server/main.js
@@ -3,9 +3,11 @@ import { Meteor } from 'meteor/meteor';
 // Collections
 import { Exports } from '../imports/api/exports.js';
 
+// Etat
 let i = 0;
 const intervals = {};
 
+// Constantes
 const urls = [
   "https://www.lempire.com/",
   "https://www.lemlist.com/",
@@ -13,16 +15,22 @@ const urls = [
   "https://www.lemstash.com/"
 ];
 
+// Helpers
 const randomNumber = (min, max) => Math.floor(Math.random() * (max - min) + min);
 
+// Méthodes 
 Meteor.methods({
+
+  // Lancement d'un export
   async 'export.start'() {
+
     const _id = Exports.insert({
       name: 'Export ' + (++i),
       progress: 0,
       done: false,
       url: urls[randomNumber(0,3)]
     });
+
     intervals[_id] = Meteor.setInterval(() => {
       const doc = Exports.findOne(_id);
       const progress = doc.progress + 5;
@@ -36,7 +44,10 @@ Meteor.methods({
         Meteor.clearInterval(intervals[_id]);
       }
     }, 1000);
+
   },
+
+  // Reset de la BDD
   async 'export.clean'() {
     Object.values(intervals).forEach(i => Meteor.clearInterval(i));
     if (Exports.find().count() > 0) {
